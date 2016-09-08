2846d55af7b11f8b332e6c1957777543dfab46b2
Draldo <draldo_lopez@hotmail.com>
2016-09-07 22:57:36 -0400

    Observable + observer + map

--- app/src/main/java/com/example/draldo/postrxjava/MainActivity.java
+++ app/src/main/java/com/example/draldo/postrxjava/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.draldo.postrxjava;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import rx.Observable;
+import rx.Subscriber;
+import rx.functions.Action1;
+import rx.functions.Func1;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Observable.just("Hello, world!")
+                .map(new Func1<String, Integer>() {
+                    @Override
+                    public Integer call(String s) {
+                        return s.hashCode();
+                    }
+                })
+                .map(new Func1<Integer, String>() {
+                    @Override
+                    public String call(Integer integer) {
+                        return Integer.toString(integer);
+                    }
+                })
+                .subscribe(new Action1<String>() {
+                    @Override
+                    public void call(String s) {
+                        System.out.println("TAG " + s);
+                    }
+                });
+
     }
 }
